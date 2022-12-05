f47c7e987c10779cf6a84aaf353b3c6ecb207da1
Fer Torres <fertorresmx@gmail.com>
2022-12-05 11:19:26 -0600

    Create a Helpers file and a new test

--- phpunit/tests/Unit/Helpers/EmailTest.php
+++ phpunit/tests/Unit/Helpers/EmailTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Helpers;
 
+use App\Helpers\Email;
 use PHPUnit\Framework\TestCase;
 
 class EmailTest extends TestCase
@@ -11,10 +12,12 @@ class EmailTest extends TestCase
      *
      * @return void
      */
+
     public function test_Email()
     {
-        $email = 'i@admin.com';
-        $result = (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
+        // $email = 'i@admin.com';
+        // $result = (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
+        $result = Email::validate('i@admin.com');
 
         $this->assertTrue($result);
     }
